79ac82030ddb4fd3261b1a531b16a8a976690f79
Michelle Polinsky <michelle.polinsky@gmail.com>
2019-08-13 16:28:11 -0400

    weather page initial commit

--- public/main.js
+++ public/main.js
@@ -1,7 +1,26 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let city = document.querySelector('.typed')
+
+const createProperSearch = () => {
+  let typedName = city.value
+  const url = `http://api.openweathermap.org/data/2.5/weather`
+  const apikey = '2048ce2abc2770f983e18462cfa5007a'
+  const newUrl =
+    url + '?' + 'q=' + typedName + '&units=imperial' + '&appid=' + apikey
+  return newUrl
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const getConditions = () => {
+  fetch(createProperSearch())
+    .then(response => {
+      return response.json()
+    })
+    .then(city => {
+      console.log(city)
+      document.querySelector('.conditions').textContent = city.weather[0].main
+      document.querySelector('.city-temps').textContent = city.main.temp
+    })
+}
+
+document.querySelector('.search').addEventListener('click', getConditions)
+
+// document.addEventListener('DOMContentLoaded', main)
